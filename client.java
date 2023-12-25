bb5ff3cdcc94ed670e9ab6e23ddec381870058ac
BlackBazaar <umut_kuruluk@hotmail.com>
2023-12-25 20:15:14 +0300

    First commit

---
+++ client.java
@@ -0,0 +1,54 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class client {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedWriter bufferedWriter = null;
+        BufferedReader bufferedReader = null;
+        Data data;
+
+        Socket socket1 = new Socket("localhost",7001);
+        System.out.println("Connected with Server");
+
+        System.out.print("Enter your name: ");
+        String name = scan.nextLine();
+        data = new Data(name,socket1.getLocalAddress(), socket1.getPort());
+
+
+        inputStreamReader = new InputStreamReader(socket1.getInputStream());
+        outputStreamWriter = new OutputStreamWriter(socket1.getOutputStream());
+
+
+
+        //Message Stream
+        bufferedWriter= new BufferedWriter(outputStreamWriter);
+        bufferedReader = new BufferedReader(inputStreamReader);
+
+
+        //Information Stream
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket1.getOutputStream());
+        objectOutputStream.writeObject(data);
+
+        while (true){
+
+            System.out.print(data.getName()+":");
+            String message = scan.nextLine();
+
+            bufferedWriter.write(message);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            if (message.equalsIgnoreCase("bye")){
+                socket1.close();
+                break;
+            }
+
+            System.out.println("Server:" + bufferedReader.readLine());
+        }
+
+    }
+}
---
+++ server.java
@@ -0,0 +1,77 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Data implements Serializable{
+    private String name;
+    private InetAddress ip;
+    private int port;
+    public Data(String name, InetAddress ip, int port){
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public InetAddress getIp() {
+        return ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
+public class server {
+
+    public static void main(String[] args) throws Exception{
+        Scanner scan = new Scanner(System.in);
+        InputStreamReader inputStreamReader= null;
+        OutputStreamWriter outputStreamWriter= null;
+        BufferedWriter bufferedWriter = null;
+        BufferedReader bufferedReader = null;
+
+        ServerSocket socket = new ServerSocket(7001);
+
+
+
+        while (true){
+            System.out.println("waiting for connection...");
+            Socket ss = socket.accept();
+
+
+            inputStreamReader = new InputStreamReader(ss.getInputStream());
+            outputStreamWriter = new OutputStreamWriter(ss.getOutputStream());
+
+            //Message Stream
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+            bufferedReader = new BufferedReader(inputStreamReader);
+
+            //Information Stream
+            ObjectInputStream objectInputStream = new ObjectInputStream(ss.getInputStream());
+            Data data = (Data)objectInputStream.readObject();
+
+            System.out.println(data.getName()+" has connected");
+
+            while (true){
+
+                String clientmessage = bufferedReader.readLine();
+                System.out.println(data.getName()+":"+clientmessage);
+
+                if (clientmessage.equalsIgnoreCase("bye")){
+                    System.out.println(data.getName()+" has disconnected");
+                    break;
+                }
+
+                System.out.print("Server:");
+                String servermessage = scan.nextLine();
+
+                bufferedWriter.write(servermessage);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        }
+    }
+}
